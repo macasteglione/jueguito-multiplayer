6b18b44f16a8d977f736faaac798d29a1a84714a
macasteglione <matiascasteglione2002@gmail.com>
2025-05-13 16:00:40 -0800

    remove network manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "include/Player.h"
 #include "include/Enemy.h"
-#include "include/NetworkManager.h"
 
 int main()
 {
@@ -10,30 +9,23 @@ int main()
     InitWindow(screenWidth, screenHeight, "Jueguito");
     SetTargetFPS(60);
 
-    NetworkManager network(NetworkRole::CLIENT);
-    network.Start();
-
     Player *player = new Player(Vector2{screenWidth / 2, screenHeight / 2});
     Enemy *enemy = new Enemy(Vector2{400, 400});
 
     while (!WindowShouldClose())
     {
         player->Update();
-        network.SetOwnPosition(player->GetPosition());
-
-        enemy->SetPosition(network.GetOtherPosition());
+        enemy->FollowTarget(player->GetPosition());
 
         BeginDrawing();
         {
             ClearBackground(BLACK);
             player->Draw();
             enemy->Draw();
-            // enemy->FollowTarget(player->GetPosition());
         }
         EndDrawing();
     }
 
-    network.Stop();
     CloseWindow();
     return 0;
 }
